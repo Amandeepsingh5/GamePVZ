b5f36f49b0599ad46d262e804effb291f1ceb1f2
Amandeepsingh5 <34280127+Amandeepsingh5@users.noreply.github.com>
2018-10-26 21:06:45 -0400

    Commenting and Indentation Followed

--- GAMEPVZ/src/BasicZoombie.java
+++ GAMEPVZ/src/BasicZoombie.java
@@ -1,5 +1,5 @@
 public class BasicZoombie extends Zoombie {
-
+	
 	public BasicZoombie() {
 		super("BasicZoombie", 50, 20);
 		// TODO Auto-generated constructor stub
--- GAMEPVZ/src/Zoombie.java
+++ GAMEPVZ/src/Zoombie.java
@@ -1,18 +1,17 @@
 import java.util.*;
 
 /**
- * This class creates different type of zoombies
+ * This class creates different type of Zombies
  * 
  * @author zoha mehdi
  *
  */
 public class Zoombie {
 	
-	private String description; //Type of zoombie
-	private int health; //Zoombie health
-	private int lawn_distance; //distance Zoombie has to cover to get the house 
-	
-	private ArrayList<String> zoombieArr = new ArrayList<>(); //Stores zoombies
+	private String description; //Type of zombie
+	private int health; //zombie health
+	private int lawn_distance; //distance zombie has to cover to get the house 
+	private ArrayList<String> zoombieArr = new ArrayList<>(); //Stores zombies
 	private int attack;
 
 	/**
@@ -24,23 +23,17 @@ public class Zoombie {
 		this.description = description;
 		this.health = health;
 		this.attack = attack;
-		
-		
-		
 	}
-	/**]
-	 * gets a zoombies health
+	/**
+	 * This method returns the Zomie Health
 	 * @return
 	 */
 	public int getHeath() {
-		
 		return health;
 	}
 	
-
 	public void decrementHealth(int attack) {
-		health -= attack;
-		
+		health -= attack;	
 	}
 	
 	public void attack(Zoombie zoombie, int health) {
