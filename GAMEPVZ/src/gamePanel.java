56855add3ad9d2e375d0fbfbf0fc0554f557611a
Amandeep Singh <34280127+Amandeepsingh5@users.noreply.github.com>
2018-11-14 15:10:47 -0500

    The override for update method added to gamePanel

--- GAMEPVZ/src/gamePanel.java
+++ GAMEPVZ/src/gamePanel.java
@@ -19,6 +19,30 @@ public class gamePanel extends mainPanel implements Observer {
 		//the buttons do nothing
 	}
 	
+public void update(Observable arg0, Object arg1) {
+		int x=0;
+		int y=0;
 
+		CreateLevel newLevel = ((CreateLevel)arg0);
+		
+		//draw each Character added to the level
+		for(Character a: newLevel.showLevel().getCharacterList()){
+			if(newLevel.showLevel().fieldDimension(x, y)){
+				
+				super.buttons[y][x].setIcon(new ImageIcon(a.getPicture()));
+				
+			}
+			x++;
+			
+			if(x > newLevel.showLevel().getMaxX()){
+				x = 0;
+				y++;
+				if(y > newLevel.showLevel().getMaxY()){
+					y = 0;
+				}
+			}
+		}
+		
+	}
 	
 }
