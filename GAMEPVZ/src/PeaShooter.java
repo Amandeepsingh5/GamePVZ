5ca2577f9feccff1dd6ce6643185bd97cd8fa26e
zohamehdi <43473034+zohamehdi@users.noreply.github.com>
2018-11-25 22:30:25 -0500

    Update PeaShooter.java

--- GAMEPVZ/src/PeaShooter.java
+++ GAMEPVZ/src/PeaShooter.java
@@ -1,43 +1,40 @@
 package model;
 
+@SuppressWarnings("serial")
 public class PeaShooter extends Plant {
 	// Default Health FCharacter multiplies with level to increase max health (Experimental)
-	private static final int Health = 40;
+	private static final int HF = 40;
 	// Default Damage FCharacter multiplies with level to increase damage (Experimental)
-	private static final int Damage = 10; 
+	private static final int DF = 10; 
 	// The solar cost of the plant
-	private static final int price = 2;
+	private static final int COST = 2;
 	// Default Sprite for the Plant
-	private static final String picture = "images/Peashooter.png";
+	private static final String picture = "images/peashooter.png";
 	
 	/**
 	 * @param level
 	 */
 	public PeaShooter(int level) {
-		super((Health * level), level, "S", price,picture);
+		super((HF * level), level, "S", COST, picture);
 	}
 	
 	/**
-	 * @return 2 if zombie successfully attacked else returns 0
+	 * @return 2 if zombie was successfully attacked else returns 0
 	 */
 	public int action(Level grid) {
-		
 		return attack(grid);
 	}
 	
 	/**
-	 * attack() iterates through the grid to check if there is any zombie in the vicinity
+	 * Moves through the grid to check if there is any zombies in the vicinity
 	 * @return 2 if zombie successfully attacked else returns 0
 	 */
 	private int attack(Level grid) {
 		// iterates through the grid to till it reaches the end of the grid
 		int x = this.x;
 		while(grid.fieldDimension(x, this.y)){
-			
-			if(grid.loc_Zombie(x, this.y)) {
-				
-				grid.getCharacterAt(x, this.y).takeDamage(Damage * super.level);
-				
+			if(grid.loc_zombie(x, this.y)) {
+				grid.getCharacterAt(x, this.y).takeDamage(DF * super.level);
 				return 2;
 			}
 			x++;
