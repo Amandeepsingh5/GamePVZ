233b839aef5dbafd4645b659ffa3abb478714299
zohamehdi <43473034+zohamehdi@users.noreply.github.com>
2018-11-25 22:39:42 -0500

    Update ExplosiveZombieTest.java

--- GAMEPVZ/src/ExplosiveZombieTest.java
+++ GAMEPVZ/src/ExplosiveZombieTest.java
@@ -1,42 +1,45 @@
 package tests;
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
 
 import model.ExplosiveZombie;
 import model.Level;
+
 import model.PeaShooter;
+
 import model.SunFlower;
+
+import org.junit.Before;
+import org.junit.Test;
 
 
 public class ExplosiveZombieTest {
-	private ExplosiveZombie actor;
+	private ExplosiveZombie explosiveZombie;
 	private Level level;
 
 	@Before
 	public void setUp() throws Exception{
-		actor = new ExplosiveZombie(1);
+		explosiveZombie = new ExplosiveZombie(1);
 		level = new Level(1);
-		level.addChar(actor, level.getMaxX() -1, 0);
+		level.addChar(explosiveZombie, level.getMaxX() -1, 0);
 	}
 	/**
-	 * Tests Act() in ExplosiveZombie and also private methods
-	 * move() and attack()
+	 * Tests Action() in ExplosiveZombie
+	 * and move() and attack()
 	 */
 	@Test
-	public void testAct() {
+	public void testAction() {
 		SunFlower plant = new SunFlower(1);
 		level.addChar(plant,level.getMaxX() - 2, 0);
-		assertTrue(2 == this.actor.action(level));
-		assertTrue(plant.getMaxHealth() > plant.getCurrHealth());
+		assertTrue(2 == this.explosiveZombie.action(level));
+		assertTrue(plant.getmaximumHealth() > plant.getcurrentHealth());
 	}
 
 	@Test
 	public void testMove(){
-		assertTrue(1 == actor.action(level));
-		actor.setXY(0, 0);
-		assertTrue(-1 == actor.action(level));
+		assertTrue(1 == explosiveZombie.action(level));
+		explosiveZombie.setXY(0, 0);
+		assertTrue(-1 == explosiveZombie.action(level));
 
 	}
 	@Test
@@ -45,9 +48,9 @@ public class ExplosiveZombieTest {
 		level.addChar(plant,level.getMaxX() - 2, 0);
 		PeaShooter anotherPlant = new PeaShooter(1);
 		level.addChar(anotherPlant, level.getMaxX() - 2, 1);
-		actor.takeDamage((int) (0.83 * actor.getMaxHealth()));
-		assertTrue(2 == actor.action(level));
-		assertTrue(!actor.isAlive());
+		explosiveZombie.takeDamage((int) (0.83 * explosiveZombie.getmaximumHealth()));
+		assertTrue(2 == explosiveZombie.action(level));
+		assertTrue(!explosiveZombie.isAlive());
 		assertTrue(!plant.isAlive());
 		assertTrue(!anotherPlant.isAlive());
 	}
