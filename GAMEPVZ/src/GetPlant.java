8e89d75d87da99db51a5df45ca12ac488210a23e
zohamehdi <43473034+zohamehdi@users.noreply.github.com>
2018-11-25 22:26:53 -0500

    Update GetPlant.java

--- GAMEPVZ/src/GetPlant.java
+++ GAMEPVZ/src/GetPlant.java
@@ -2,22 +2,24 @@ package model;
 
 import java.io.Serializable;
 
-
-public class GetPlant {
+@SuppressWarnings("serial")
+public class GetPlant implements Cloneable, Serializable{
 	private int level;
-	
+
 	public GetPlant(int level){
 		this.level = level;
 	}
 	
 	/**
-	 * 
-	 * @param string
-	 * @param suns
+	 * Checks if string matches a plant name and if the are enough sun points
+	 * @param string - a string that defines what type of plant it is
+	 * @param sunpoints - the current amount of sun points
 	 * @return requested Plant if it exists else returns null
 	 */
-	public Plant getPlant(String string, int suns) {				//should use an enum instead of a string...
+	public Plant getPlant(String string, int sunpoints) {
+		//should use an enum instead of a string...
 		Plant plant = null;
+		
 		if(string.equals("sunflower")){
 			plant = new SunFlower(level);
 		}
@@ -25,18 +27,28 @@ public class GetPlant {
 		else if (string.equals("shooter")){
 			plant = new PeaShooter(level);
 		}
+		else if(string.equals("snowshooter")){
+			plant = new SnowShooter(level);
+		}
 		
+		else if(string.equals("walnut")){
+			plant = new Walnut(level);
+		}
 		else{
 			return null;
 		}
-		// Check if there are enough suns for the plant
-		if(plant.price() > suns){
+		// Check if there are enough sunpoints for the plant
+		if(plant.getprice() > sunpoints){
 			return null;
 		}
 		else{
 			return plant;
 		}
 	}
-}
 
-	
+	public Object clone() throws CloneNotSupportedException{
+		GetPlant clone = (GetPlant)super.clone();
+		clone.level = this.level;
+		return clone;
+	}
+}
