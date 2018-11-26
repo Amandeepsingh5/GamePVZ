54fc4593a13e1772da54a5c1acfba44f28328401
zohamehdi <43473034+zohamehdi@users.noreply.github.com>
2018-11-25 22:27:53 -0500

    Update Level.java

--- GAMEPVZ/src/Level.java
+++ GAMEPVZ/src/Level.java
@@ -4,109 +4,99 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 
+
 @SuppressWarnings("serial")
 public class Level implements Cloneable, Serializable{
+	//The dimensions of the level
 	public static final int M_Y = 6;
 	public static final int M_X = 12;
 
 	private ArrayList<Character> CharacterList;
-
 	private ArrayList<Character> comingZombiesList;
 	//grid may be redundant. All the positional information in it is also stored in CharacterList.
 	private int level;
 
-	public ArrayList<Character> getCharacterList() {
-
-		return CharacterList;
-	}
-
-
 	public Level(int level) {
-
 		this.level = level;
 		CharacterList = new ArrayList<Character>();
 		comingZombiesList = new ArrayList<Character>();
 
 		for(int x = 0; x < (5 + level); x++){
-
-			getcomingZombiesList().add(new ExplosiveZombie(level)); 			//add some basic zombies
-
+			getcomingZombiesList().add(new BasicZombie(level)); 			//add some basic zombies
 		}
-
-
+		for(int x = 0; x < (level); x++){
+			getcomingZombiesList().add(new PoleZombie(level)); 			//add some pole-vault zombies
+		}
+		for(int x = 0; x < (level+2); x++){
+			getcomingZombiesList().add(new ExplosiveZombie(level)); 	//add some exploding zombies
+		}
 	}
 
+	/**
+	 * Returns the list of all the Characters
+	 * @return CharacterList -a list of all the Characters
+	 */
+	public ArrayList<Character> getCharacterList() {
+		return CharacterList;
+	}
 
 	/**
 	 * Gets all Characters that are currently alive. We don't remove dead Characters, just sort of ignore them.
 	 * @return all Characters that are in play and alive
 	 */
 	public ArrayList<Character> getLivingCharacters(){
-		ArrayList<Character> alive = new ArrayList<Character>();
+		ArrayList<Character> liveOnes = new ArrayList<Character>();
 		for(Character a: CharacterList){
-
-			if(!a.isAlive()){
+			if(a.isAlive()){
+				liveOnes.add(a);
 			}
-			else {alive.add(a);}
 		}
-
-		return alive;
+		return liveOnes;
 	}
 
+	/**
+	 * Returns the incoming zombies for the level
+	 * @return comingZombiesList the comingZombiesList to set
+	 */
 	public ArrayList<Character> getcomingZombiesList() {
 		return comingZombiesList;
 	}
 
 	/**
+	 * Creates the incoming zombies for the level
 	 * @param comingZombiesList the comingZombiesList to set
 	 */
 	public void setcomingZombiesList(ArrayList<Character> comingZombiesList) {
 		this.comingZombiesList = comingZombiesList;
 	}
 	/**
-	 * Checks if a character is at the coordinates
+	 * Checks if there is a Character at the given coordinates
 	 * @param x coordinate
 	 * @param y coordinate
-	 * @return true if there is an Character at the given coordinates
+	 * @return true if there is a Character at the given coordinates
 	 */
 	public boolean CharacterAt(int x, int y){
+
 		for(Character a: CharacterList){
+
 			if(a.isAt(x, y) && a.isAlive()){
+
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public int getMaxY() {
-
-		int max= 	M_Y;
-		return max;
-	}
 
 	/**
-	 * @return the maxX
-	 */
-	public int getMaxX() {
-		int maxx = M_X;
-		return maxx;
-	}
-
-	/**
-	 * Checks if a zombie is at the coordinates
+	 * Checks the coordinates for an alive Zombie
 	 * @param x coordinate
 	 * @param y coordinate
-	 * @return true if there is a zombie at the coordinate
+	 * @return true if there is an alive zombie at the coordinate
 	 */
-
-	public boolean loc_Zombie(int x, int y){
-
+	public boolean loc_zombie(int x, int y){
 		for(Character a: CharacterList){
-
 			if(a.isAt(x, y)){
-
-				if(  !a.isFriendly () && a.isAlive()){
-
+				if(!a.isPlant() && a.isAlive()){
 					return true;
 				}
 			}
@@ -117,27 +107,26 @@ public class Level implements Cloneable, Serializable{
 	/**
 	 * Adds a new Character to the level at a certain position. Position is stored in the Character, and in the grid.
 	 * Character is also enrolled in the CharacterList
-	 * @param newCharacter
-	 * @param x
-	 * @param y
-	 * @return
+	 * @param newCharacter- the new Character
+	 * @param x coordinate
+	 * @param y coordinate
+	 * @return true if the newCharacter has been set in the x, y coordinate and added to the character list
 	 */
-
+	public boolean addChar(Character newCharacter, int x, int y){
+		return(newCharacter.setXY(x, y) && CharacterList.add(newCharacter));
+	}
 
 	/**
 	 * Searches the grid for a plant Character at a position.
 	 * Because this information is also stored in Characters, a search of the CharacterList would also work
 	 * @param x coordinate
 	 * @param y coordinate
-	 * @return true if the plant is at the cooridinates and is alive, false otherwise
+	 * @return true is there is an alive plant at the coordinates
 	 */
 	public boolean plantAt(int x, int y){
 		for(Character a: CharacterList){
-
 			if(a.isAt(x,y)){
-
-				if( a.isFriendly ()&& a.isAlive()){
-
+				if(a.isPlant() && a.isAlive()){
 					return true;
 				}
 			}
@@ -145,14 +134,8 @@ public class Level implements Cloneable, Serializable{
 		return false;
 	}
 
-
-	public boolean addChar(Character newChar, int x, int y){
-
-		return(newChar.setXY(x, y) && CharacterList.add(newChar));
-	}
-
 	/**
-	 * Returns a list of all the Characters at the coordinates
+	 * Returns an ArrayList of all the Characters at the coordinates
 	 * @param x coordinate
 	 * @param y coordinate
 	 * @return a list of all the Characters at the coordinates
@@ -168,51 +151,60 @@ public class Level implements Cloneable, Serializable{
 	}
 
 	/**
-	 * Sets	the characters coordinates
+	 * 	Returns the character at a given coordinate	
 	 * @param x coordinate
 	 * @param y coordinate
 	 * @return the first Character found at the given coordinates
 	 */
 	public Character getCharacterAt(int x, int y){
-
 		for(Character a: CharacterList){
-
 			if(a.isAt(x,y)){
-
 				return(a);
-
 			}
 		}
 		return null;
 	}
 
+	/**
+	 * @return the maxY
+	 */
+	public int getMaxY() {
+		return M_Y;
+	}
 
 	/**
-	 *  Sets the field dimensions
+	 * @return the maxX
+	 */
+	public int getMaxX() {
+		return M_X;
+	}
+
+	/**
+	 * Checks if the coordinates are within the bounds of the level
 	 * @param x coordinate
 	 * @param y coordinate
 	 * @return true if the coordinates are within the bounds of the level
 	 */
 	public boolean fieldDimension(int x, int y){
-
 		return(x >= 0 && x < M_X && y >= 0 && y < M_Y);
 	}
 
+
+	/**
+	 * Clones everything about the Level
+	 * @return clone of the Level
+	 */
 	public Object clone() throws CloneNotSupportedException{
-
 		Level clone = (Level)super.clone();
-
 		clone.comingZombiesList = new ArrayList<Character>();
+		clone.CharacterList = new ArrayList<Character>();
 		for(Character a: comingZombiesList){
 			clone.comingZombiesList.add((Character) a.clone());
 		}
-		clone.CharacterList = new ArrayList<Character>();
 		for(Character a: CharacterList){
 			clone.CharacterList.add((Character) a.clone());
 		}
-
 		clone.level = this.level;
-
 		return clone;
 	}
 
